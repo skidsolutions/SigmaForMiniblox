6b3ca4200b29f8733d1a1b4ea55d7ed8d2e445bc
SkidSolutions <118870230+skidsolutions@users.noreply.github.com>
2024-08-08 15:29:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Vape V4
-// @namespace    http://7granddadpgn.github.io
+// @name         Sigma 5.0
+// @namespace    e
 // @version      2024-07-29
 // @description  A browser script made to give enhancements on miniblox.io
-// @author       7GrandDad
+// @author       7GrandDad creds
 // @match        https://miniblox.io/*
 // @icon         https://miniblox.io/favicon.png
 // @grant        GM_getValue
